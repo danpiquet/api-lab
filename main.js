e2b9789fc96f74b38b2b5199a03d958ef86ed873
Dan Piquet <dan.piquet@gmail.com>
2022-11-09 14:20:42 -0700

    initial commit of api lab

---
+++ main.js
@@ -0,0 +1,17 @@
+// const { default: axios } = require("axios")
+let button = document.querySelector('#button')
+const buttonClick = async () => {
+    let request = await axios.get('https://swapi.dev/api/planets/?search=Alderaan')
+    let residents = request.data.results[0].residents
+    for(i=0;i<residents.length;i++){
+        let body = document.querySelector('body')
+        let resRequest = await axios.get(residents[i])
+        let name = resRequest.data.name
+        console.log(name)
+        let h2 = document.createElement('h2')
+        h2.textContent = name
+        body.appendChild(h2)
+    }
+}
+
+button.addEventListener('click',buttonClick)
